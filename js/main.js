7354f391ba555cc9d52e706a9c5e516ae71c9fe4
Alexandre Rieux <alexandre.rieux@groupe-regards.com>
2015-03-06 17:45:29 +0100

    d3 skills

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ requirejs(['jquery', 'd3', 'sankey'], function ($, d3) {
     var units = "widgets";
 
     var margin = {
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20
     };
 
-    var width = 700 - margin.left - margin.right;
+    var width = 800 - margin.left - margin.right;
     var height = 300 - margin.top - margin.bottom;
 
     var sankey = d3.sankey()
@@ -39,10 +39,38 @@ requirejs(['jquery', 'd3', 'sankey'], function ($, d3) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    /**
+    *
+    * Create the drop shadow effect
+    *
+    **/
+    var defs = svg.append("defs");
+
+    var filter = defs.append("filter")
+        .attr("id", "drop-shadow")
+        .attr("height", "600%");
+
+    filter.append("feGaussianBlur")
+        .attr("in", "SourceAlpha")
+        .attr("stdDeviation", 3)
+        .attr("result", "blur");
+
+    filter.append("feOffset")
+        .attr("in", "blur")
+        .attr("dx", 3)
+        .attr("dy", 3)
+        .attr("result", "offsetBlur");
+
+    var feMerge = filter.append("feMerge");
+    feMerge.append("feMergeNode").attr("in", "offsetBlur");
+    feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+
+    /*******************************/
+
     var path = sankey.link();
 
     d3.json("js/data/skills.json", function(error, graph){
-        // debugger;
+
         sankey.nodes(graph.nodes)
               .links(graph.links)
               .layout(32);
@@ -52,17 +80,16 @@ requirejs(['jquery', 'd3', 'sankey'], function ($, d3) {
               .enter().append("path")
               .attr("class", "link")
               .attr("d", path)
-              .style("stroke-width", function(d) { // skills strongs here
-                // debugger;
+              .style("stroke-width", function(d) {
                 return Math.max(1, d.value);
               })
+              .style("filter", "url(#drop-shadow)")
               .sort(function(a, b) {
                 return b.dy - a.dy;
               });
 
         link.append("title")
             .text(function(d) {
-                // debugger;
                 return d.source.name + " → " + d.target.name + "\n" + format(d.value);
             });
     });
